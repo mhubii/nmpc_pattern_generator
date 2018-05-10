78b8cb51d34c7f4c423c09415924ed10ffc80c16
Martin Huber <m.huber_1994@hotmail.de>
2018-05-10 14:17:20 +0200

    external keyboard interface

---
+++ src/keyboard_user_interface.cpp
@@ -0,0 +1,12 @@
+#include "reader.h"
+
+int main() {
+
+    // Create a keyboard user interface.
+    KeyReader kr;
+
+    // Start reading incomming commands.
+    kr.ReadCommands();
+
+    return 0;
+}
