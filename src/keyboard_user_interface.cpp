a831a9ff5caedb9a36ded27b066c083aceb5330d
Martin Huber <m.huber_1994@hotmail.de>
2018-05-14 15:01:31 +0200

    added yarp network

--- src/keyboard_user_interface.cpp
+++ src/keyboard_user_interface.cpp
@@ -1,6 +1,11 @@
+#include <yarp/os/all.h>
+
 #include "reader.h"
 
 int main() {
+
+    // Create yarp network.
+    yarp::os::Network yarp;
 
     // Create a keyboard user interface.
     KeyReader kr;
