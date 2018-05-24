09e19d91b629e59f256548bbda61182f1e20af37
Martin Huber <m.huber_1994@hotmail.de>
2018-05-24 17:47:50 +0200

    tests

--- libs/io_module/src/test_writer.cpp
+++ libs/io_module/src/test_writer.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+#include <yarp/eigen/Eigen.h>
 #include <Eigen/Dense>
 #include <iostream>
 
@@ -7,22 +9,47 @@
 #include "writer.h"
 
 int main(int argc, char * argv[]) {
+    
+    // Set up yarp network.
     yarp::os::Network yarp;
 
-
-    int period = 10;
-    ReadJoints rj(1000);
-    WriteJoints wj(1000);
-    // ReadCameras rc(100);
-
-    rj.start();
+    // Set up writer.
+    int period = 100;
+    WriteJoints wj(period);
     wj.start();
-    //rc.start();
+
+    // Initialize position with a good guess.
+    Eigen::MatrixXd q_init(21, 3);
+    q_init.setZero();
+
+    q_init(2, 0)  = 0.6;
+    q_init(6, 0)  = 0.54;
+    q_init(9, 0)  = -0.57;
+    q_init(10, 0) = -0.23;
+    q_init(12, 0) = 0.54;
+    q_init(15, 0) = -0.57;
+    q_init(16, 0) = -0.23;
+
+    Eigen::MatrixXd q_traj = q_init.bottomRows(15);
+    std::cout << q_traj.col(0) << std::endl;
+
+    // Write joint angles to output port.
+    yarp::sig::Matrix data(q_traj.rows(), q_traj.cols());
+    Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(data.data(), q_traj.rows(), q_traj.cols()) = q_traj;
+
+    // Create /joints/read port.
+    yarp::os::BufferedPort<yarp::sig::Matrix> port_q;
+
+    port_q.open("/joints/read");
+
     yarp::os::Network::connect("/joints/read", "/joints/write");
-    yarp::os::Time::delay(10);
-    //rc.stop();
+
+    while (wj.GetInitialPositionStatus() != DONE) {
+        port_q.prepare() = data;
+        port_q.write();
+    }
+
     wj.stop();
-    rj.stop();
 
     return 0;
 }
---
+++ src/test_robot.cpp
@@ -0,0 +1,149 @@
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+#include <yarp/eigen/Eigen.h>   
+#include <Eigen/Core>
+#include <iostream>
+
+#include "utils.h"
+
+class CheckRateThread : public yarp::os::RateThread
+{
+    public: CheckRateThread(int period) : RateThread(period) {
+
+                port_.open("/check_rate_thread");
+            };
+      
+            ~CheckRateThread() { 
+
+                port_.close();
+            };
+
+    private: virtual void run() { 
+
+                yarp::sig::Vector* read = port_.read(false);
+
+                if (read != YARP_NULLPTR) {
+
+                    std::cout << yarp::eigen::toEigen(*read) << std::endl;
+                }
+
+                // double t1;
+                // double t2;
+ 
+                for (int i = 0; i < 10; i++) {
+                    
+                    // t1 = yarp::os::Time::now();
+
+                    // TODO: how to assure that the loop takes a specific time and not the delay?
+                    // t2 = yarp::os::Time::now();
+                    // yarp::os::Time::delay(0.03); // - t2 - t1);
+                }
+             };
+
+             yarp::os::BufferedPort<yarp::sig::Vector> port_;
+};
+
+int main() {
+
+    // Check a run thread that reads slower than its port gets written to.
+    yarp::os::Network yarp;
+    
+    CheckRateThread thread(200);
+
+    yarp::os::BufferedPort<yarp::sig::Vector> input_port;
+    input_port.open("/input_port");
+
+    yarp::os::Network::connect("/input_port", "/check_rate_thread");
+
+    thread.start();
+
+    for (int i = 0; i < 10; i++) {
+
+        yarp::sig::Vector input(1);
+        input(0) = i;
+
+        input_port.prepare() = input;
+        input_port.write(true);
+
+        yarp::os::Time::delay(0.2);
+    }
+
+    thread.stop();
+
+    input_port.close();
+
+
+    // Check storage order of yarp matrices.
+    // // Yarp matrix.
+    // yarp::sig::Matrix yarp(2, 2);
+    // yarp.zero();
+
+    // // Eigen matrix.
+    // Eigen::MatrixXd eigen(2, 2);
+    // eigen.setConstant(5);
+    // eigen(0, 1) = 0;
+
+    // // Copy data from Eigen to Yarp matrix.
+    // Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(yarp.data(), eigen.rows(), eigen.cols()) = eigen;
+
+    // // Show results.
+    // std::cout << "yarp:\n" << yarp::eigen::toEigen(yarp) << std::endl;
+    // std::cout << "eigen:\n" << eigen << std::endl;
+
+    // std::cout << "yarp parts:\n" << yarp(0,0) << " " << yarp(0,1) << "\n" << yarp(1,0) << " " << yarp(1,1) << std::endl;
+    // std::cout << "yarp parts:\n" << eigen(0,0) << " " << eigen(0,1) << "\n" << eigen(1,0) << " " << eigen(1,1) << std::endl;
+
+
+
+
+    // Test to connect one port to many others.
+    // yarp::os::Network yarp;
+
+    // // Buffered port to send status.
+    // yarp::os::BufferedPort<yarp::os::Bottle> port;
+    // port.open("/writer/info");
+    // yarp::os::BufferedPort<yarp::os::Bottle> port_1;
+    // port_1.open("/writer/info_1");
+    // yarp::os::BufferedPort<yarp::os::Bottle> port_2;
+    // port_2.open("/writer/info_2");
+
+    // // Connect ports.
+    // yarp::os::Network::connect("/writer/info", "/writer/info_1");
+    // yarp::os::Network::connect("/writer/info", "/writer/info_2");
+
+    // yarp::os::Bottle& bottle = port.prepare();
+    // yarp::os::Property& dict = bottle.addDict();
+
+    // dict.put("hi", 1);
+    // port.write();
+
+    // yarp::os::Bottle* bottle_1 = port_1.read();
+    // yarp::os::Value prop_1 = bottle_1->pop();
+    // std::cout << prop_1.asDict()->find("hi").asInt() << std::endl;
+
+    // yarp::os::Bottle* bottle_2 = port_2.read();
+    // yarp::os::Value prop_2 = bottle_2->pop();
+    // std::cout << prop_2.asDict()->find("hi").asInt() << std::endl;
+
+    // // Send status.
+    // yarp::os::Bottle& bottle = port.prepare();
+    // yarp::os::Property& dict = bottle.addDict();
+
+    // dict.put("InitialPositionStatus", DONE);
+    // port.write();
+    // yarp::os::Time::delay(2);
+
+    // dict.put("InitialPositionStatus", MOVING);
+    // port.write();
+    // yarp::os::Time::delay(2);
+
+    // dict.put("InitialPositionStatus", DONE);
+    // port.write();
+    // yarp::os::Time::delay(2);
+
+    // port.close();
+    // port_1.close();
+    // port_2.close();
+
+    return 0;
+}
