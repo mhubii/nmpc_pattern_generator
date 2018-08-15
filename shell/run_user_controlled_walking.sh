abe420c68fbed71d27c3db7680baa9b6ce848e32
mhubii <m.huber_1994@hotmail.de>
2018-08-15 15:13:14 +0200

    shell scripts

--- shell/run_user_controlled_walking.sh
+++
@@ -1,8 +0,0 @@
-# User controlled walking shell script.
-
-# Location of shell script.
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-export YARP_CLOCK=/clock
-cd $DIR/../build
-./user_controlled_walking --io_config ../libs/io_module/configs.yaml --pg_config ../libs/pattern_generator/configs.yaml --ki_config ../libs/kinematics/configs.yaml --robot icubGazeboSim
