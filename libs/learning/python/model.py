ed215ff631ddba082baae62d2623901a86b34fbd
martin <m.huber_1994@hotmail.de>
2019-07-01 17:26:15 +0200

    sovled merge conflicts

--- libs/learning/python/model.py
+++ libs/learning/python/model.py
@@ -105,27 +105,12 @@ class RGBDCNNLSTM(nn.Module):
 
         super(RGBDCNNLSTM, self).__init__()
 
-<<<<<<< HEAD
-        self.conv1 = nn.Conv2d(4, 64, 5, 2)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(64, 64, 3, 1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 128, 3, 1)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.conv4 = nn.Conv2d(128, 128, 3, 1)
-        self.bn4 = nn.BatchNorm2d(128)
-        self.conv5 = nn.Conv2d(128, 256, 3, 1)
-        self.bn5 = nn.BatchNorm2d(256)
-        self.conv6 = nn.Conv2d(256, 256, 3, 1)
-        self.bn6 = nn.BatchNorm2d(256)
-=======
         # Convolutional blocks.
         self.conv_block1 = ConvBlock(4, 64, 2)
         self.conv_block2 = ConvBlock(64, 128, 2)
         self.conv_block3 = ConvBlock(128, 128, 2)
         self.conv_block4 = ConvBlock(128, 128, 2)
         self.conv_block5 = ConvBlock(128, 128, 2)
->>>>>>> 33aa3364653b1ed1911ebb72ad416622e377595b
 
         n = self._get_conv_output(input_shape)
 
@@ -144,20 +129,11 @@ class RGBDCNNLSTM(nn.Module):
 
         # Unsqueeze to obtain 1x(shape) as dimensions.
         x = torch.rand(shape).unsqueeze(0)
-<<<<<<< HEAD
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.conv6(x)
-=======
         x = self.conv_block1(x)
         x = self.conv_block2(x)
         x = self.conv_block3(x)
         x = self.conv_block4(x)
         x = self.conv_block5(x)
->>>>>>> 33aa3364653b1ed1911ebb72ad416622e377595b
         n = x.numel()
         return n
 
@@ -170,20 +146,11 @@ class RGBDCNNLSTM(nn.Module):
         x = x.view(b*t, c, h, w)
 
         # Convolutional layers for feature extraction.
-<<<<<<< HEAD
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = F.relu(self.bn4(self.conv4(x)))
-        x = F.relu(self.bn5(self.conv5(x)))
-        x = F.relu(self.bn6(self.conv6(x)))
-=======
         x = self.conv_block1(x)
         x = self.conv_block2(x)
         x = self.conv_block3(x)
         x = self.conv_block4(x)
         x = self.conv_block5(x)
->>>>>>> 33aa3364653b1ed1911ebb72ad416622e377595b
 
         # Flatten.
         x = x.view(b*t, -1)
@@ -193,11 +160,7 @@ class RGBDCNNLSTM(nn.Module):
         x = torch.tanh(self.fc2(x))
         x = x.view(b, t, -1)
         x, _ = self.rnn(x)
-<<<<<<< HEAD
-        x = torch.tanh(self.fc3(x[:, -1, :])) # return last time step
-=======
         x = torch.tanh(self.fc3(x))
->>>>>>> 33aa3364653b1ed1911ebb72ad416622e377595b
 
         return x
 
